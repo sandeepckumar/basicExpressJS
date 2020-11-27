a95a667ff9ae1b0f1d08c9b751b71465796c8cfd
Chenna <sandeep.chenna@cotiviti.com>
2020-11-27 12:00:21 +0530

    cleaned up

--- routes/file.route.js
+++ routes/file.route.js
@@ -1,56 +1,51 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path")
-const { readDir, writeFile, fileAccess, readFile, appendFile, deleteFile } = require("../promisifiedFs/fileIO")
+const path = require("path");
+const { readDir, writeFile, fileAccess, readFile, appendFile, deleteFile } = require("../promisifiedFs/fileIO");
 
 router.get("/show", async (req, res) => {
-    let dirPath = path.resolve("files")
+    let dirPath = path.resolve("files");
     try
     {
-    var files = await readDir(dirPath)
-    console.log(files)
-    res.status(200).json({"Show available files": files})
+        var files = await readDir(dirPath);
+        res.status(200).json({ "Show available files": files });
     } catch (err)
     {
-        console.error(err)
-        res.status(503).json({error:"Error reading DIR"})
+        res.status(503).json({ error: "Error reading DIR" });
     }
 })
 
 router.get("/read/:fileName", async (req, res) => {
-    let fileName = req.params['fileName']
+    let fileName = req.params['fileName'];
     try
     {
-        let filePath = path.resolve(`files/${fileName}`)
-        console.log(filePath)
-        await fileAccess(filePath)
-        let data = await readFile(filePath)
-        res.status(200).send(data)
+        let filePath = path.resolve(`files/${fileName}`);
+        await fileAccess(filePath);
+        let data = await readFile(filePath);
+        res.status(200).send(data);
     } catch (err)
     {
-        console.error(err)
-        res.status(401).send(`Error: ${err}`)
+        res.status(401).send(`Error: ${err}`);
     }  
    
 })
 
 router.post("/write", async (req, res) => {
-    console.log(req.body)
     if (req.body.name && req.body.data)
     {
         try
         {
-            let fileName = path.resolve(`files/${req.body.name}`)
-            await writeFile(fileName, req.body.data)
-            res.status(201).send(`File ${req.body.name} has been created. Use "GET" on /file/readFile/${req.body.name} to read the file contents.`)
+            let fileName = path.resolve(`files/${req.body.name}`);
+            await writeFile(fileName, req.body.data);
+            res.status(201).send(`File ${req.body.name} has been created. Use "GET" on /file/readFile/${req.body.name} to read the file contents.`);
         } catch (err)
         {
-            res.status(500).send(`Error occured:\n ${err}`)
+            res.status(500).send(`Error occured:\n ${err}`);
         }
         
     } else
     {
-        res.status(401).send("Please send body in the following manner:\n{\n'name':'Enter name of the file',\n'data':'Enter data to be written to the file'\n} ")
+        res.status(401).send("Please send body in the following manner:\n{\n'name':'Enter name of the file',\n'data':'Enter data to be written to the file'\n} ");
     }
 })
 
@@ -60,18 +55,18 @@ router.put("/append", async (req, res) => {
     {
         try
         {
-            let fileName = path.resolve(`files/${req.body.name}`)
+            let fileName = path.resolve(`files/${req.body.name}`);
             await fileAccess(fileName);
-            await appendFile(fileName, req.body.data)
-            res.status(201).send(`The file has been updated. Use "GET" on /file/readFile/${req.body.name} to read the file contents.`)
+            await appendFile(fileName, req.body.data);
+            res.status(201).send(`The file has been updated. Use "GET" on /file/readFile/${req.body.name} to read the file contents.`);
 
         } catch (err)
         {
-            res.send(`Error Occured:\n ${err}`)
+            res.send(`Error Occured:\n ${err}`);
         }
     } else
     {
-        res.status(401).send("Please send body in the following manner:\n{\n'name':'Enter name of the file',\n'data':'Enter data to be written to the file'\n} ")
+        res.status(401).send("Please send body in the following manner:\n{\n'name':'Enter name of the file',\n'data':'Enter data to be written to the file'\n} ");
     }
 })
 
@@ -83,15 +78,15 @@ router.delete("/delete", async (req, res) => {
             let fileName = path.resolve(`files/${req.body.name}`);
             await fileAccess(fileName);
             await deleteFile(fileName);
-            res.status(201).send(`The file "${req.body.name}" is delete. Use "GET" on /file/show to see available files.`)
+            res.status(201).send(`The file "${req.body.name}" is delete. Use "GET" on /file/show to see available files.`);
 
         } catch (err)
         {
-            res.status(401).send(`Error Occured:\n${err}`)
+            res.status(401).send(`Error Occured:\n${err}`);
         }
     } else
     {
-        res.status(401).send("Please send body in the following manner:\n\n{\n'name':'Enter name of the file',\n'} ")
+        res.status(401).send("Please send body in the following manner:\n\n{\n'name':'Enter name of the file',\n'} ");
     }
     
 })
